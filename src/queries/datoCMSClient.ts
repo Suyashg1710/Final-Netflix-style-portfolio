e26bda0224fb93eb060b54a8ca7d1828e6eb4c06
Suyashg1710 <suyashg1710@gmail.com>
2026-01-10 16:17:49 +0100

    Update datoCMSClient.ts

--- src/queries/datoCMSClient.ts
+++ src/queries/datoCMSClient.ts
@@ -1,5 +1,4 @@
 import { GraphQLClient } from 'graphql-request';
-import { getDatoCmsToken } from './getDatoCmsToken';
 
 const DATO_CMS_ENDPOINT = 'https://graphql.datocms.com/';
 const DATO_CMS_API_TOKEN = '401f1b917a6de7852f4ce16ce1456e';
