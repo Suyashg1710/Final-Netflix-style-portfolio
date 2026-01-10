e868edf7b9c8bb24d3472e9ba8b64e789126a6a1
Suyashg1710 <suyashg1710@gmail.com>
2026-01-10 22:11:16 +0100

    Update HireMePage2.tsx
    Currentslide deleted

--- src/pages/HireMePage2.tsx
+++ src/pages/HireMePage2.tsx
@@ -12,7 +12,7 @@ import "slick-carousel/slick/slick-theme.css";
 import "./HireMePage2.css";
 
 const HireMePage: React.FC = () => {
-const [_currentSlide, setCurrentSlide] = useState<number>(0);
+const [, setCurrentSlide] = useState<number>(0);
 
 
   // Your photos for the slider - UPDATE THESE PATHS
